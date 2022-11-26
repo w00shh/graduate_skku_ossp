dba2710d0c179198d63e0168cb8ba212785fa76b
haltsh <113428746+haltsh@users.noreply.github.com>
2022-11-26 17:08:23 +0900

    Add files via upload

---
+++ normal_check.py
@@ -0,0 +1,25 @@
+#일반선택교양은 구분이 없어 단일리스트로 생성
+
+normalChoice = ['KMOOC01', 'KMOOC02', 'KMOOC03', 'KMOOC04', 'KMOOC05', 'KMOOC06', '4차산업혁명과스타트업창업', '4차산업혁명의이해와진로탐색', 'FYE세미나Ⅰ', 'FYE세미나II',
+'교실밖창업과실무기초', '교양골프', '교양농구', '교양댄스스포츠', '교양배드민턴', '교양소프트볼', '교양수영', '교양스노우보드', '교양스키', '교양요가', '교양축구',
+'교양탁구', '교양테니스', '교양플로어볼', '글로벌문화체험세미나', '대학원생활과연구', '대학한국어세미나', '리더십과경영', '메타버스비즈니스', '사진예술의이해',
+'사회봉사론', '새로운패러다임의취업/창업', '성균인성수양', '성균프레시맨세미나', '성균프레시맨세미나2', '속기학', '수학적사고', '스토리텔링과트랜스미디어의이해',
+'실용금융', '언어논리입문', '언어지문분석', '여성커리어개발', '예절교육및실습', '외국인대학원생을위한학술적글쓰기', '자료해석과상황판단', '정보통신윤리', '정보통신현장실습6',
+'종합한국어1', '종합한국어2', '종합한국어3', '종합한국어4', '종합한국어5', '종합한국어6', '진로탐색및경력개발', '창업과비즈니스모델', '창업과프랜차이즈', '창업비즈니스사업계획',
+'창업비즈니스아이디어', '창업시제품제작 ', '창업실습1', '창업실습2', '창업현장실습1', '창업현장실습2', '창업현장실습3', '창업현장실습4', '창업현장실습5', '창업현장실습6',
+'창의융합캡스톤디자인', '창의인성교육의이해', '추리논증방법', '컴퓨터문서작성', '컴퓨터자료처리', '테크노컬쳐리더십', '플라잉디스크', '한국생활의안내및진로지도1', '핵심취업전략',
+'현장실습1', '현장실습2', '현장실습3', '형식적논리학']
+
+normalChoiceEn = ['KMOOC01', 'KMOOC02', 'KMOOC03', 'KMOOC04', 'KMOOC05', 'KMOOC06', 'The Fourth Industrial Revolution and Start-up Business Basic', 'The Fourth Industrial Revolution and Career Exploration',
+'First Year Experience Seminar Ⅰ', 'FYE Seminar II', 'Out-of-class Start-up and Practical Theory', 'Golf', 'Basketball', 'Dance sport', 'Badminton', 'Softball', 'Swimming', 'Snow Board', 'Ski', 'Yoga',
+'Soccer', 'Table tennis', 'Tennis', 'Floorball', 'Seminar for Experience of Global Culture and Entrepreneurship', 'Graduate life and research', 'Academic Korean Seminar', 'Leadership & Business',
+'Introduction to Metaverse Business', 'Comprehension of Photographic Art', 'Voluntarism and Entrepreneurship', 'Employment/Start-up of New Paradigm', 'SKKU Personality Cultivation', 'Freshman Seminar and Entrepreneurship',
+'Freshman Seminar and Entrepreneurship2', 'Stenography', 'Mathematical Thinking', 'Storytelling and Understanding Transmedia', 'Practical Finance', 'Linguistic Analysis', 'Analysis of Passages',
+'Career Development and Entrepreneurship for Female Students', 'Theory and Practice on Good Manners', 'Academic writing for international graduate students', 'Data Analysis and Situational Judgement',
+'Information Communication Ethics', 'Field Training Program 6 for School of Information and Communication', 'Regular Korean Program 1', 'Regular Korean Program 2', 'Regular Korean Program 3',
+'Regular Korean Program 4', 'Regular Korean Program 5', 'Regular Korean Program 6', 'Career Exploration and Development with Entrepreneurship', 'Business Model for Startup', 'Entrepreneurship and Franchise System',
+'Creating Business Plan', 'Idea to Business Model', 'Prototyping', 'Start-up Activity 1', 'Start-up Activity 2', 'Start-up Field Practice 1', 'Start-up Field Practice 2', 'Start-up Field Practice 3',
+'Start-up Field Practice 4', 'Start-up Field Practice 5', 'Start-up Field Practice 6', 'Creative Convergence Capstone Design', 'Knowing Creativity and Character Education', 'Method of Reasoning and Argumentation',
+'Computer Word Processing', 'Computer Data Processing', 'Technoculture Leadership', 'Flying Disc', 'Guidance of Living and Career Development in Korea 1', 'Core Strategy for Employment and Startup',
+'Co-op Program1', 'Co-op Program 2', 'Co-op Program 3', 'Formative Thinking']
+
