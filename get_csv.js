eec6fbca581936a45b18daf2853524fe76e9402d
W00SHH <jerry7780@g.skku.edu>
2022-11-28 01:19:26 +0900

    change get_csv.js

--- get_csv.js
+++ get_csv.js
@@ -1,41 +1,38 @@
 // xlsl to csv 출처 : https://github.com/SheetJS/js-xlsx/issues/214
 function excelExport(event) {
-    excelExportCommon(event, handleExcelDataAll);
+  excelExportCommon(event, handleExcelDataAll);
+}
+function excelExportCommon(event, callback) {
+  var input = event.target;
+  var reader = new FileReader();
+  reader.onload = function () {
+    var fileData = reader.result;
+    var wb = XLSX.read(fileData, { type: "binary" });
+    var sheetNameList = wb.SheetNames; // 시트 이름 목록 가져오기
+    var firstSheetName = sheetNameList[0]; // 첫번째 시트명
+    var firstSheet = wb.Sheets[firstSheetName]; // 첫번째 시트
+    callback(firstSheet);
+  };
+  reader.readAsBinaryString(input.files[0]);
+}
+function handleExcelDataAll(sheet) {
+  handleExcelDataCsv(sheet); // csv 형태
+}
+function handleExcelDataCsv(sheet) {
+  //$("#displayExcelCsv").html(XLSX.utils.sheet_to_csv(sheet));
+  let split_with_enter = XLSX.utils.sheet_to_csv(sheet).split("\n");
+  let subject_list = [];
+  for (let i = 2; i < split_with_enter.length; i = i + 2) {
+    subject_list.push(split_with_enter[i]);
   }
-  function excelExportCommon(event, callback) {
-    var input = event.target;
-    var reader = new FileReader();
-    reader.onload = function () {
-      var fileData = reader.result;
-      var wb = XLSX.read(fileData, { type: "binary" });
-      var sheetNameList = wb.SheetNames; // 시트 이름 목록 가져오기
-      var firstSheetName = sheetNameList[0]; // 첫번째 시트명
-      var firstSheet = wb.Sheets[firstSheetName]; // 첫번째 시트
-      callback(firstSheet);
-    };
-    reader.readAsBinaryString(input.files[0]);
+  for (let i in subject_list) {
+    subject_list[i] = subject_list[i].split(",");
   }
-  function handleExcelDataAll(sheet) {
-    handleExcelDataCsv(sheet); // csv 형태
-  }
-  function handleExcelDataCsv(sheet) {
-    //$("#displayExcelCsv").html(XLSX.utils.sheet_to_csv(sheet));
-    let split_with_enter = XLSX.utils.sheet_to_csv(sheet).split("\n");
-    let subject_list = [];
-    for (let i = 2; i < split_with_enter.length; i = i + 2) {
-      subject_list.push(split_with_enter[i]);
-    }
-    for (let i in subject_list) {
-      subject_list[i] = subject_list[i].split(",");
-    }
-    subject_list = subject_list.reduce(function (acc, cur) {
-      return acc.concat(cur);
-    });
-    localStorage.setItem("subject_list", JSON.stringify(subject_list));
-    document.getElementById("csv_result").innerHTML = subject_list.join("<br>");
-  }
-  
-  function submit_form() {
-    $("#id_result").text($("#ID_input").val());
-  }
-  
+  console.log(subject_list);
+  localStorage.setItem("subject_list", JSON.stringify(subject_list));
+  document.getElementById("csv_result").innerHTML = subject_list.join("<br>");
+}
+
+function submit_form() {
+  $("#id_result").text($("#ID_input").val());
+}
